ece8564ef12908a4384ee65a7906cec0a7db23e1
Zoey Kaiser <zoeykaiser8@gmail.com>
2024-12-21 00:02:34 +0100

    Added Favicon and moved all content to app.vue

--- app.vue
+++ app.vue
@@ -1,5 +1,16 @@
+<script lang="ts" setup>
+useHead({ title: 'Averix' })
+</script>
+
 <template>
-  <div>
-    <NuxtPage />
-  </div>
+  <LandingBackground>
+    <div class="relative flex flex-col items-center justify-center gap-4 px-4">
+      <div class="flex items-center gap-2">
+        <Logo class="fill-black h-[140px] w-[140px] dark:fill-white" />
+        <div class="text-center text-3xl font-bold md:text-9xl dark:text-white">
+          Averix
+        </div>
+      </div>
+    </div>
+  </LandingBackground>
 </template>
