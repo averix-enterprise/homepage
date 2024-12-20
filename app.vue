1ba89e020a2c8f5c9bbdcd1a6770851df1e069af
Zoey <zoeykaiser8@gmail.com>
2024-12-21 00:15:41 +0100

    feat: add initial landing page and github pages deployment (#1)

--- app.vue
+++ app.vue
@@ -1,5 +1,16 @@
+<script lang="ts" setup>
+useHead({ title: 'Averix' })
+</script>
+
 <template>
-  <div>
-    <NuxtPage />
-  </div>
+  <LandingBackground>
+    <div class="relative flex flex-col items-center justify-center gap-4 px-4">
+      <div class="flex items-center gap-2">
+        <Logo class="fill-black h-[50px] w-[50px] md:h-[140px] md:w-[140px] dark:fill-white" />
+        <div class="text-center text-4xl font-bold md:text-9xl dark:text-white">
+          Averix
+        </div>
+      </div>
+    </div>
+  </LandingBackground>
 </template>
--- pages/index.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="grid h-screen w-full place-content-center">
-    <h1 class="text-2xl font-semibold">
-      Homepage coming soon
-    </h1>
-  </div>
-</template>
