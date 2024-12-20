d2a844de1bcaf8a917ad73d2e4569f30f77ba282
Zoey Kaiser <zoeykaiser8@gmail.com>
2024-12-21 00:03:26 +0100

    mobile fixes

--- app.vue
+++ app.vue
@@ -6,8 +6,8 @@ useHead({ title: 'Averix' })
   <LandingBackground>
     <div class="relative flex flex-col items-center justify-center gap-4 px-4">
       <div class="flex items-center gap-2">
-        <Logo class="fill-black h-[140px] w-[140px] dark:fill-white" />
-        <div class="text-center text-3xl font-bold md:text-9xl dark:text-white">
+        <Logo class="fill-black h-[50px] w-[50px] md:h-[140px] md:w-[140px] dark:fill-white" />
+        <div class="text-center text-4xl font-bold md:text-9xl dark:text-white">
           Averix
         </div>
       </div>
