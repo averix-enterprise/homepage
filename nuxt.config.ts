fbce38ed9403aa7b528847548c65302d88ab2e12
Zoey Kaiser <zoeykaiser8@gmail.com>
2024-12-28 15:20:49 +0100

    move to nuxt v4 folder structure and code cleanup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  compatibilityDate: '2024-08-19',
+  future: { compatibilityVersion: 4 },
   modules: [
     '@nuxtjs/tailwindcss',
     '@vueuse/motion/nuxt'
   ],
-  compatibilityDate: '2024-08-19',
-  devtools: {
-    enabled: true
-  },
   typescript: {
     shim: false
   }
